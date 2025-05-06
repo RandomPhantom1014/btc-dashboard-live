babda09a4e880c8aabf69efda02065afddfe3d46
RandomPhantom1014 <Kahalid312shakur@gmail.com>
2025-05-05 21:52:25 -1000

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-from dashboard import app
+import dash
+from dashboard import layout, register_callbacks
 
-if __name__ == "__main__":
-    app.run_server(debug=True, host="0.0.0.0", port=8080)
+app = dash.Dash(__name__, suppress_callback_exceptions=True)
+app.title = "BTC Signal Dashboard"
+app.layout = layout
+server = app.server
+
+register_callbacks(app)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
