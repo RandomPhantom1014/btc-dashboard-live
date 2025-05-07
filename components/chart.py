7341a2e4a141b451a85bb9fbb3e5542bb9f41ca8
RandomPhantom1014 <Kahalid312shakur@gmail.com>
2025-05-07 08:21:35 -1000

    Update chart.py

--- components/chart.py
+++ components/chart.py
@@ -1,13 +1,27 @@
 # components/chart.py
 
-from dash import dcc, html
+import plotly.graph_objs as go
+from dash import dcc
+import pandas as pd
 
-def build_price_chart():
-    return html.Div(className="chart-container", children=[
-        html.H2("BTC Price Chart"),
-        dcc.Graph(
-            id="btc-candlestick-chart",
-            config={"displayModeBar": False},
-            style={"height": "400px"}
-        )
-    ])
+def render_candlestick_chart(df: pd.DataFrame):
+    fig = go.Figure(
+        data=[
+            go.Candlestick(
+                x=df['time'],
+                open=df['open'],
+                high=df['high'],
+                low=df['low'],
+                close=df['close'],
+                name="BTC/USD"
+            )
+        ]
+    )
+    fig.update_layout(
+        xaxis_title='Time',
+        yaxis_title='Price (USD)',
+        template='plotly_dark',
+        margin=dict(l=40, r=40, t=40, b=40),
+        height=400
+    )
+    return dcc.Graph(figure=fig, id="candlestick-chart")
