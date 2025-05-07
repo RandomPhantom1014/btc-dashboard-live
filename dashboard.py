cfc9026b2319129299c09894995bf50632169077
RandomPhantom1014 <Kahalid312shakur@gmail.com>
2025-05-07 05:18:35 -1000

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,13 +1,19 @@
 # dashboard.py
 
 import dash
+from dash import html
 import dash_bootstrap_components as dbc
 from components.layout import create_layout
 from components.callbacks import register_callbacks
+from components.theme import register_theme_callbacks
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.title = "BTC Signal Dashboard"
 app._favicon = "btc.ico"
-app.layout = create_layout()
+
+app.layout = create_layout(app)
+
 register_callbacks(app)
+register_theme_callbacks(app)
+
 server = app.server
