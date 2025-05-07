85467ad85eea2382be15f8a0904b8bf8c5c1c6a0
RandomPhantom1014 <Kahalid312shakur@gmail.com>
2025-05-06 23:43:31 -1000

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,8 @@
 # app.py
 
 from dashboard import app
-from components.callbacks import register_callbacks
-from components.layout import layout
 
-# Set the layout
-app.layout = layout()
-
-# Register all callbacks
-register_callbacks(app)
-
-# Server exposure for deployment (e.g., Render)
 server = app.server
 
-# Local development entry point
 if __name__ == "__main__":
-    app.run_server(debug=False, host="0.0.0.0", port=8080)
+    app.run_server(debug=True)
