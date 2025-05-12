f92cc9b452d6b0eafb0b6da468b88eb126bb9876
RandomPhantom1014 <Kahalid312shakur@gmail.com>
2025-05-12 08:01:24 -1000

    Update header.py

--- header.py
+++ header.py
@@ -1,1 +1,16 @@
+from dash import html
+
+def render_header():
+    return html.Div(
+        id='header',
+        style={'textAlign': 'center', 'marginBottom': '20px'},
+        children=[
+            html.H1('BTC Signal Dashboard', style={'marginBottom': '5px'}),
+            html.Div(id='btc-price-text', style={
+                'fontSize': '24px',
+                'fontWeight': 'bold',
+                'color': '#2a9d8f'
+            }),
+        ]
+    )
 
