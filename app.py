d2bba6caf40cbaaced9df2cdca3a2190782ea203
RandomPhantom1014 <Kahalid312shakur@gmail.com>
2025-05-15 13:24:39 -1000

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 import dash
-from dash import dcc, html
 from layout import serve_layout
 from callbacks import register_callbacks
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True)
-app.title = 'BTC Signal Dashboard'
+app.title = "XRP Signal Dashboard"
 app.layout = serve_layout
 server = app.server
 
 register_callbacks(app)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
