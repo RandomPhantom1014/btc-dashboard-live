d4588c3980675471da96634f130b4e32ebd7626f
RandomPhantom1014 <Kahalid312shakur@gmail.com>
2025-05-15 20:32:29 -1000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from dash import Dash, html, dcc
-import dash_bootstrap_components as dbc
 from layout import serve_layout
+import dash_bootstrap_components as dbc
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 server = app.server
